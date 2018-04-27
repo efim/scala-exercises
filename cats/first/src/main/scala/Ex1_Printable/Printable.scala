b57f63a6c433ee4fd2ea13286b577f9368eb31c5
Efim Nefedov <efim666@bk.ru>
2018-04-27 10:13:13 +0300

    Ops to use printable operations directly on values of supported classes

--- cats/first/src/main/scala/Ex1_Printable/Printable.scala
+++ cats/first/src/main/scala/Ex1_Printable/Printable.scala
@@ -6,7 +6,14 @@ trait Printable[A] {
 
 object Printable {
   def format[A](value: A)(implicit proof: Printable[A]): String = proof.format(value)
-  def print[A: Printable](value: A): Unit = println(implicitly[Printable[A]].format(value))
+  def print[A](value: A)(implicit proof: Printable[A]): Unit = println(format(value))
+}
+
+object PrintableSyntax {
+  implicit class PrintableOps[A](value: A) {
+    def format(implicit proof: Printable[A]): String = proof.format(value)
+    def print(implicit proof: Printable[A]): Unit = println(proof.format(value))
+  }
 }
 
 object PrintableInstances {
