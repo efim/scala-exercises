4517367ddfb7e561e8e607042d2b285fee24685d
Efim Nefedov <efim666@bk.ru>
2018-08-29 20:21:31 +0300

    instance of cats.Eq for Cat for typesafe comparison

--- cats/first/src/main/scala/Ex1_Printable/Cat.scala
+++ cats/first/src/main/scala/Ex1_Printable/Cat.scala
@@ -1,6 +1,10 @@
 package Ex1_Printable
 
 import cats.Show
+import cats.Eq
+import cats.syntax.eq._
+import cats.instances.int._
+import cats.instances.string._
 
 final case class Cat(name: String, age: Int, color: String)
 
@@ -13,5 +17,11 @@ object Cat {
   implicit val catShow: Show[Cat] =
     Show.show(cat => s"${cat.name} is a ${cat.age} year old ${cat.color} cat.")
 
+  implicit val catEq: Eq[Cat] =
+    Eq.instance( (leftCat, rightCat) =>
+      (leftCat.name === rightCat.name)
+        && (leftCat.age === rightCat.age)
+        && (leftCat.color === rightCat.color)
+    )
 }
 
